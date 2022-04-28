987500614507725992f455be4a91c2711974c5fb
YusufAtesmen <yusufatesmen@gmail.com>
2022-04-29 02:58:06 +0300

    Geometric Calculation Project

--- src/geometrikHesaplama/Cember.java
+++ src/geometrikHesaplama/Cember.java
@@ -3,16 +3,11 @@ package geometrikHesaplama;
 public  class Cember extends Sekil {
 
 
-
-
-
-
-
     @Override
     public void cemberAlan(double a) {
 
 
-        System.out.println("Cemberin alani : "+3.14*a*a);
+        System.out.println("Cemberin alani : "+(3.14*a*a));
     }
 
 
